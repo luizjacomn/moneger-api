12b4602f3630b463b00f50fccadd4bc851e8b034
Luiz Jacó <luizjacomn@gmail.com>
2018-09-10 23:16:14 -0300

    primeiro commit

---
+++ src/main/java/com/luizjacomn/moneger/api/MonegerApiApplication.java
@@ -0,0 +1,12 @@
+package com.luizjacomn.moneger.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonegerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonegerApiApplication.class, args);
+	}
+}
