2eead1e4e1da7f0bbb9efadde0bd8d7f808ba7ba
Luiz Jacó <luizjacomn@gmail.com>
2018-09-12 21:37:37 -0300

    Criando o recurso categoria - parte 01

---
+++ src/main/java/com/luizjacomn/moneger/api/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.luizjacomn.moneger.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luizjacomn.moneger.api.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+}
---
+++ src/main/java/com/luizjacomn/moneger/api/resource/CategoriaResource.java
@@ -0,0 +1,50 @@
+package com.luizjacomn.moneger.api.resource;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.luizjacomn.moneger.api.model.Categoria;
+import com.luizjacomn.moneger.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public List<Categoria> buscarTodos() {
+		return repository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = repository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(categoriaSalva.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{id}")
+	public Categoria buscarPorId(@PathVariable Long id) {
+		return repository.getOne(id);
+	}
+}
